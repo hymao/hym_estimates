5c7715f0cad2b14ae05baaba14b9eb240fbe38bd
Matt Yoder <diapriid@gmail.com>
2018-08-02 16:17:42 -0500

    updated data; prioritizing sources

--- go.rb
+++ go.rb
@@ -1,21 +1,37 @@
 require 'csv'
 require 'byebug'
 
-EST_PRIORITY = {
- 'est_sp_JH' => 'John Heraty, 2018',
- 'est_sp_JF' => 'Jose Fernandez, 2018', 
- 'est_sp_UCD' => 'Universal Chalcidoidea Database, accessed 2018',
- 'est_sp_HW' => 'Hymenoptera of the World',
- 'est_sp_Wikipedia' => 'Wikipedia, accessed 2018',
- 'est_sp_Jones_2009' => 'Jones, 2009',
- 'est_sp_JH_2002' => 'JH 2002' 
+EST_PRIORITY = %w{ 
+  est_sp_JH
+  est_sp_JH_2002
+  est_sp_UCD
+  est_sp_JF
+  est_sp_Jones_2009
+  est_sp_HW
+  est_sp_Wikipedia
+} 
+
+DESC_PRIORITY = %w{
+  desc_species_other
+  desc_species_AG
+  desc_species_WP
 }
 
-DESC_PRIORITY = { 
+EST_SOURCES = {
+  'est_sp_JH' => 'John Heraty, 2018',
+  'est_sp_JFT' => 'José L. Fernández-Triana, 2018', 
+  'est_sp_UCD' => 'Universal Chalcidoidea Database, accessed 2018',
+  'est_sp_HW' => 'Hymenoptera of the World',
+  'est_sp_Wikipedia' => 'Wikipedia, accessed 2018',
+  'est_sp_Jones_2009' => 'Jones, Owen R., et al. "Using taxonomic revision data to estimate the geographic and taxonomic distribution of undescribed species richness in the Braconidae (Hymenoptera: Ichneumonoidea)." Insect Conservation and Diversity 2.3 (2009): 204-212.',
+  'est_sp_JH_2002' => 'Heraty, John Michael. Revision of the genera of Eucharitidae (Hymenoptera: Chalcidoidea) of the world. American Entomological Institute, 2002.' 
+}
+
+DESC_SOURCES = { 
   # 'desc_genera_WP' => '',
-  'desc_species_WP' => 'WP',
+  'desc_species_WP' => 'Wikipedia/Wikispecies',
   # 'desc_genera_AG' => '',
-  'desc_species_AG' => 'AG', 
+  'desc_species_AG' => 'Aguiar, Alexandre P., et al. "Order hymenoptera." Zootaxa 3703.1 (2013): 51-62.', 
   # 'desc_genera_other' => '',
   'desc_species_other' => ''
 }
@@ -33,11 +49,20 @@ def get_values(row, mode = :est)
         raise
       end 
 
-  v = row.select{|c| a.keys.include?(c.first) && c.last.to_i > 0}
-  a.keys.each do |p|
+  b = case mode 
+      when :est
+        EST_SOURCES
+      when :desc
+        DESC_SOURCES
+      else
+        raise
+      end 
+
+  v = row.select{|c| a.include?(c.first) && c.last.to_i > 0}
+  a.each do |p|
     v.each do |d|
       if d.first == p
-        cit = a[p]
+        cit = b[p]
         cit = row['other_reference'] if mode == :desc && cit == 'desc_species_other'
         return [d.last, cit] 
       end
@@ -47,13 +72,15 @@ def get_values(row, mode = :est)
 
 end
 
+# Generate the TSV
+
 data.each do |row|
   puts [ 
     'Hymenoptera',
     row['Family'],
     nil,
-    get_values(row),
     get_values(row, :desc),
+    get_values(row),
   ].flatten.join("\t")
 
 end
